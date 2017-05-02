57108fec6a9fa34d95de710d416a39a84e6f73a6
obeaulieu <beaul068@umn.edu>
2017-05-02 12:38:51 -0500

    Update Data_organization.py

--- Data_organization.py
+++ Data_organization.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """
-Created on Tue Feb 28 17:57:20 2017
+Created on Mon May  1 21:29:36 2017
 
 @author: livbeaulieu30
 """
+
 
 # Data organization
 # Using a netCDF to organize photos and scan data from DB1
@@ -26,38 +27,45 @@ print (nc.file_format)
 #gtif = gdal.Open( '/Users/livbeaulieu30/Google Drive/Scan Files/*.DAT' )
 #print gtif.GetMetadata()
 # a = gtif.GetRasterBand(1).ReadAsArray()
-datafiles = glob('/Users/livbeaulieu30/Google Drive/Scan Files/*.DAT')
+datafiles = glob('*.DAT')
 
-t_scan = []
-for datafile in datafiles:
-    z1D = np.fromfile(datafile, dtype=np.float32)
-    z = np.reshape(z1D, (-1, 3936))
-    z[z == -9999] = np.nan # no data handling
-    z = np.flipud(z) # flip top to bottom
-    #plt.imshow(z)
-    #plt.show()
-    print datafile
-    t_scan.append(int(datafile.split('TopoDat_')[1].split('.')[0]))
-
-                                                       
 ndim = 9450336
 xdimension = range(0, 3936)
 ydimension = range(0, 2401) 
 #zdimension = 35
                                                         
 #dimensions
-time = nc.createDimension('time', None)
-nc.createDimension('x', len(xdimension))
-nc.createDimension('y', len(ydimension))
+#time = nc.createDimension('time', None)
+x = nc.createDimension('x', len(xdimension))
+y = nc.createDimension('y', len(ydimension))
+time = nc.createDimension('time', len(datafiles))
 #nc.createDimension('z', zdimension)
                                                         
 #variables
-time = nc.createVariable('time', 'f8', ('time',))
 x = nc.createVariable('x', 'f4', ('x',))
 y = nc.createVariable('y', 'f4', ('y',))
+time = nc.createVariable('time', 'f4', ('time',))
 #z = nc.createVariable('z', 'f4', ('z',))
-field = nc.createVariable('field', 'f8', ('time', 'x', 'y'))
-                                                        
+nc.createVariable('field', 'f4', ('y', 'x', 'time'))
+
+t_scan = []
+i = 0
+for datafile in datafiles:
+    z1D = np.fromfile(datafile, dtype=np.float32)
+    z = np.reshape(z1D, (-1, 3936))
+    z[z == -9999] = np.nan # no data handling
+    z = np.flipud(z) # flip top to bottom
+    nc.variables['field'][i] = z
+    #plt.imshow(z)
+    #plt.show()
+    print datafile
+    t_scan.append(int(datafile.split('TopoDat_')[1].split('.')[0]))
+    i += 1
+
+nc.variables['x'][:] = np.arange(0, len(x)*1E-3, 1E-3)
+nc.variables['y'][:] = np.arange(0, len(y)*1E-3, 1E-3)
+nc.variables['time'][:] = np.array(t_scan)
+
 x.units = 'Meters'
 y.units = 'Meters'
 
